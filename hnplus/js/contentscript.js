980390b320ee63fd954db0828e0256e6c561061b
Mayank Singhal <manku.singhal@gmail.com>
2011-12-26 06:26:06 +0530

    Completed the first version of the application. Works.

--- hnplus/js/contentscript.js
+++ hnplus/js/contentscript.js
@@ -3,142 +3,208 @@
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
-
-var HandleJAFPages = function() {
-	$.tablesorter.addParser({ 
-		// set a unique id 
-		id: 'jafDates', 
-		is: function(s) { 
-				// return false so this parser is not auto detected 
-				return false; 
-		}, 
-		format: function(s) { 
-				// format your data for normalization
-				var data = s.split("-");
-				return data[2] + data[1] + data[0]; 
-		}, 
-		// set type, either numeric or text 
-		type: 'numeric' 
-	}); 
-	var extraTables = $("<table width='100%' class='meta'></table>" +
-	  	"<div class='buttons'></div>" +
-			"<table width='100%' class='jafData'><thead><tr></tr></thead><tbody></tbody></table>" +
-			"<table width='100%' class='meta2'></table>");
-	$("table").attr('class', 'old');
-	$("table.old").before(extraTables);
-	var metaMoveCount = 0;
-	var headingMoved = false;
-	$("table.old tr").each(function() {
-		var tdCount = $(this).find("td").length;
-		if (tdCount !== 13) {
-			$(this).detach();
-			if (metaMoveCount < 5) {
-				$("table.meta").append($(this));	
-			} else {
-				$("table.meta2").append($(this));	
-			}
-			metaMoveCount++;
-		} else {
-			// Find if "Not Eligible!!" JAF
-			var notEligible = $(this).text().indexOf("Not Eligible!!") !== -1;
-			var isClosed = $(this).text().indexOf("Jaf Closed") !== -1;
-			var signedAleady = $(this).text().indexOf("signed (click to unsign)") !== -1;
-			if (headingMoved) {
-				if (notEligible) {
-					$(this).addClass("notEligible");	
-				} else if (signedAleady) {
-					$(this).addClass("signedAlready");
-				} else if (isClosed) {
-					$(this).addClass("jafClosed");
+var HNPlus = function() {
+	
+	/**
+	 * Implements keyboard shortcuts for the current page.
+	 */
+	var keyboardShortcuts = function() {
+		
+		/**
+		 * Returns a story Object given corresponding DOM.
+		 * 
+		 * @param domElement The DOM Element corresponding to the story.
+		 * @return Correspoding story Object.
+		 */
+		var story = function(row1, row2, row3) {
+			var isSelected = false;
+			var url = (typeof row1.find("td:last-child a").attr("href") === "undefined") ?
+					row2.find("td:last-child a").attr("href") : row1.find("td:last-child a").attr("href");
+			/**
+			 * Selects the story.
+			 */
+			var selectStory = function() {
+				if (isSelected) {
+					return;
 				}
-				$("table.jafData tbody").append($(this));
-			} else {
-				// Move into heading
-				$(this).find("td").each(function() {
-					var th = $("<th>" + $(this).html() + "</th>");
-					$("table.jafData thead tr").append(th);
-				})
-				headingMoved = true;
+				isSelected = true;
+				
+				row1.addClass("selected");
+				row1.addClass("top");
+				row2.addClass("selected");
+				row2.addClass("bottom");
+				
+				var offset = row1.offset().top - $(window).scrollTop();
+				if((offset > window.innerHeight) || (offset < 0)){
+					$('html, body').animate({
+							scrollTop: row1.offset().top
+						}, { queue: false, duration: 600});
+				}
+			};
+			
+			/**
+			 * Deselects the story.
+			 */
+			var deselectStory = function() {
+				if (!isSelected) {
+					return;
+				}
+				isSelected = false;
+				row1.removeClass("selected");
+				row1.removeClass("top");
+				row2.removeClass("selected");
+				row2.removeClass("bottom");
+			};
+			return {
+				select : selectStory,
+				deselect : deselectStory,
+				isSelected : function() {
+					return isSelected;
+				},
+				openHere : function() {
+					window.location = url;
+				},
+				openInNew : function() {
+					window.open(url);
+				}
+			}
+		};
+		
+		var stories = [];
+		var currentlySelected = -1;
+		
+		/**
+		 * Deselects the previous story, selects the new one.
+		 *
+		 * @param newIndex Index of the new story to be selected.
+		 */
+		var selectElement = function(newIndex) {
+			if (newIndex == currentlySelected) {
+				return;
+			}
+			stories[currentlySelected].deselect();
+			stories[newIndex].select();
+			currentlySelected = newIndex;
+		}
+		
+		/**
+		 * Select the story that comes prior to the current story
+		 * 
+		 */
+		var selectPrevious = function() {
+			if (currentlySelected == 0) {
+				return;
+			}
+			selectElement(currentlySelected - 1);
+		}
+		
+		/**
+		 * Select the story that comes after to the current story
+		 * 
+		 */
+		var selectNext = function() {
+			if (currentlySelected == stories.length - 1) {
+				return;
+			}
+			selectElement(currentlySelected + 1);
+		}
+		
+		/**
+		 * Selects the first story.
+		 */
+		var selectFirst = function() {
+			selectElement(0);
+		}
+		
+		/**
+		 * Selects the last story.
+		 */
+		var selectLast = function() {
+			selectElement(stories.length - 1);
+		}
+		
+		/**
+		 * Open Story in the current Tab.
+		 */
+		var openStoryHere = function() {
+			stories[currentlySelected].openHere();
+		}
+		
+		/**
+		 * Open Story in the a New Tab.
+		 */
+		var openStoryInNew = function() {
+			stories[currentlySelected].openInNew();
+		}
+		
+		/**
+		 * Parses DOM to get an array of story Objects.
+		 */
+		var parseStories = function () {
+			var rowElements = [];
+			$("table").each(function(index) {
+				if (index != 2) {
+					return;
+				}
+				$(this).find("tr").each(function () {
+					rowElements.push($(this));
+					$(this).addClass("genericRow");
+					// $(this).text();
+				});
+			});
+			for (var i = 0; i < rowElements.length / 3; i++) {
+				stories.push(story(rowElements[3 * i], rowElements[3 * i + 1], rowElements[3 * i + 2]));
+			}
+			stories[0].select();
+			currentlySelected = 0;
+			
+			// Register events.
+			$(document).bind("keydown", "k", selectPrevious);
+			$(document).bind("keydown", "j", selectNext);
+			$(document).bind("keydown", "home", selectFirst);
+			$(document).bind("keydown", "end", selectLast);
+			$(document).bind("keydown", "m", openStoryHere);
+			$(document).bind("keydown", "n", openStoryInNew);
+			$(document).bind("keydown", "return", openStoryHere);
+			$(document).bind("keydown", "shift+return", openStoryInNew);
+		};
+		
+		parseStories();
+	};
+	
+	/**
+	 * Checks if the string starts with any of the given prefixes.
+	 *
+	 * @param string String to be searched.
+	 * @param prefixes Array of string prefixes.
+	 * @return True if string starts with any of the prefixes, false otherwise.
+	 */
+	var hasPrefix = function (string, prefixes) {
+		for (var id in prefixes) {
+			if (string.indexOf(prefixes[id]) == 0) {
+				return true;
 			}
 		}
-	});
-	// Make THEAD and TBODY
+		return false;
+	};
 	
-	$("table.jafData").tablesorter({
-		sortList: [[7,0]],
-		cssAsc: "customHeaderSortUp",
-		cssDesc: "customHeaderSortDown",
-		cssHeader: "customHeader",
-		//debug: true,
-		headers: {
-			7: {
-				sorter: "jafDates"
-			}
-		}
-	});
-	var notEligibleVisible = false;
-	var signedVisible = true;
-	var showClosedJafs = false;
-	if (!notEligibleVisible) {
-		$("tr.notEligible").slideUp();
-	}
-	if (!signedVisible) {
-		$("tr.signedAleady").slideUp();
-	}
-	if (!showClosedJafs) {
-		$("tr.jafClosed").slideUp();
-	}
-	$("div.buttons").append($("<button>Show Not Eligible</button>").click(function() {
-		if (notEligibleVisible) {
-			$("tr.notEligible").slideUp();
-			$(this).html("Show Not Eligible");
-			notEligibleVisible = false;
-		} else {
-			$("tr.notEligible").slideDown();
-			$(this).html("Hide Not Eligible");
-			notEligibleVisible = true;
-		}
-	}));
-	$("div.buttons").append($("<button>Show Closed JAFs</button>").click(function() {
-		if (showClosedJafs) {
-			$("tr.jafClosed").slideUp();
-			$(this).html("Show Closed JAFs");
-			showClosedJafs = false;
-		} else {
-			$("tr.jafClosed").slideDown();
-			$(this).html("Hide Closed JAFs");
-			showClosedJafs = true;
-		}
-	}));
 	
-	$("div.buttons").append($("<button>Hide Signed</button>").click(function() {
-		if (signedVisible) {
-			$("tr.signedAlready").slideUp();
-			$(this).html("Show Signed");
-			signedVisible = false;
-		} else {
-			$("tr.signedAlready").slideDown();
-			$(this).html("Hide Signed");
-			signedVisible = true;
-		}
-	}));
-	
-}
-
-function DoPlacementAwesomeness(data) {
-	
-	$(document).ready(function() {
+	/**
+	 * Initiate operations: Keyboard Shortcuts etc.
+	 */
+	var initate = function() {
 		var pathname = window.location.pathname;
-		if ((pathname === "/placements/studjaf4stud.jsp") ||
-				(pathname === "/placements/studsign2.jsp") ||
-				(pathname === "/placements/studunsign2.jsp")) {
-			// All JAFs Page
-			HandleJAFPages();
-		} else if (pathname.indexOf("studjafview.jsp") !== -1) {
-			// JAF View Page
-			$("body").addClass("jafView");
+		
+		// Keyboard shortcuts.
+		var prefixes = [ "/news", "/newest", "/x"];
+		if (hasPrefix(pathname, prefixes)) {
+			// Operations for news pages.
+			keyboardShortcuts();
 		}
+	};
+	
+	$(document).ready(function () {
+		initate();
 	});
-}
-DoPlacementAwesomeness();
+} ();
 
